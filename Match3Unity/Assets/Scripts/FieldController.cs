a1b36009c561bc44b472af95c0b1c87614aa5c17
Aleks <a481024937z@gmail.com>
2019-08-30 02:11:44 +0300

    Added: field generator, tiles swap

---
+++ Match3Unity/Assets/Scripts/FieldController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour
+{
+    public static FieldController instance;
+
+    public Tile[][] tilesOnField;
+
+    private Tile selectedTile;
+    private GameObject highlightFxObject;
+
+    [SerializeField] private Tile prefabTile;
+    [SerializeField] private GameObject prefabHighlight;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+            instance = this;
+
+        if (highlightFxObject == null)
+        {
+            highlightFxObject = Instantiate(prefabHighlight);
+            highlightFxObject.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        GenerateNewField(9);
+    }
+
+    public void GenerateNewField(int size)
+    {
+        transform.position = new Vector2(-size/2f + 0.5f, -size/2f + 0.5f);
+        tilesOnField = new Tile[size][];
+        for(int i = 0; i < size; i++)       //Create columns
+        {
+            tilesOnField[i] = new Tile[size];
+            for(int j = 0; j < size; j++)   //Generate tiles in current column
+            {
+                tilesOnField[i][j] = Instantiate(prefabTile, transform);
+                tilesOnField[i][j].transform.localPosition = new Vector2(i, j);
+                tilesOnField[i][j].Reset((TileColor)Random.Range(0, 5), new Vector2(i,j));
+            }
+        }
+    }
+
+    public void ClickOn(Tile tile)
+    {
+        if (selectedTile == tile)
+        {
+            selectedTile = null;    //Deselect earlier selected tile
+            highlightFxObject.SetActive(false);
+        }
+        else
+        {
+            if (selectedTile == null || 
+                ((Mathf.Abs(selectedTile.adress.x - tile.adress.x) > 1 || selectedTile.adress.y != tile.adress.y) &&
+                (Mathf.Abs(selectedTile.adress.y - tile.adress.y) > 1 || selectedTile.adress.x != tile.adress.x)))
+            {
+                selectedTile = tile;
+                highlightFxObject.transform.position = tile.transform.position;
+                highlightFxObject.SetActive(true);                
+            }
+            else
+            {
+                SwapTiles(selectedTile, tile);
+                selectedTile = null;
+                highlightFxObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SwapTiles(Tile first, Tile second)
+    {
+        tilesOnField[(int)first.adress.x][(int)first.adress.y] = tilesOnField[(int)second.adress.x][(int)second.adress.y];
+        tilesOnField[(int)second.adress.x][(int)second.adress.y] = tilesOnField[(int)first.adress.x][(int)first.adress.y];
+
+        Vector2 tmp = first.adress;
+        first.adress = second.adress;
+        second.adress = tmp;
+
+        first.transform.localPosition = first.adress;
+        second.transform.localPosition = second.adress;
+    }
+}
---
+++ Match3Unity/Assets/Scripts/Tile.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileColor
+{
+    Cyan,
+    Magenta,
+    Yellow,
+    Blue,
+    Orange
+}
+
+public class Tile : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Vector2 adress;
+
+    [SerializeField] public TileColor tileColor;
+    [SerializeField] private Sprite spriteCyan;
+    [SerializeField] private Sprite spriteMagenta;
+    [SerializeField] private Sprite spriteYellow;
+    [SerializeField] private Sprite spriteBlue;
+    [SerializeField] private Sprite spriteOrange;
+
+    [Space]
+    [SerializeField] private ParticleSystem BurstParticle;
+
+    public void Start()
+    {
+        SetColor((TileColor)Random.Range(0, 5));
+    }
+
+    public void Reset(TileColor color, Vector2 adress)
+    {
+        SetColor(color);
+        this.adress = adress;
+    }
+
+    private void SetColor(TileColor color)
+    {
+        if (spriteRenderer == null) spriteRenderer = GetComponent<SpriteRenderer>();
+
+        tileColor = color;
+        switch (color)
+        {
+            case TileColor.Cyan:
+                spriteRenderer.sprite = spriteCyan;
+                break;
+            case TileColor.Magenta:
+                spriteRenderer.sprite = spriteMagenta;
+                break;
+            case TileColor.Yellow:
+                spriteRenderer.sprite = spriteYellow;
+                break;
+            case TileColor.Blue:
+                spriteRenderer.sprite = spriteBlue;
+                break;            
+            case TileColor.Orange:            
+                spriteRenderer.sprite = spriteOrange;
+                break;
+
+            default:
+                Debug.LogWarning(gameObject.name + " - invalid tile type! Set to default");
+                tileColor = TileColor.Cyan;
+                spriteRenderer.sprite = spriteCyan;
+                break;
+        }        
+    }
+
+    public void Burst()
+    {
+        Destroy(gameObject, 0.5f);
+    }
+
+    private void OnMouseDown()
+    {
+        FieldController.instance.ClickOn(this);
+    }
+}
